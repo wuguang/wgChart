ffd146ec7546bf6b2d40fae27a7a670f465ebb64
“wuguang” <wuguang@163.com>
2025-05-15 21:27:10 +0800

    init program

---
+++ js/common.js
@@ -0,0 +1,25 @@
+const common = {
+	getFileJsonData:(name)=>{
+		const filePath = `_www/static/my-json/${name}`;
+		return new Promise((resovle,reject)=>{
+			try{
+				plus.io.requestFileSystem( plus.io.PRIVATE_WWW, function(fs){
+					// fs.root是根目录操作对象DirectoryEntry
+					fs.root.getFile(`static/my-json/${name}`,{create:true}, function(fileEntry){
+						fileEntry.file( function(file){
+							var fileReader = new plus.io.FileReader();
+							fileReader.readAsText(file, 'utf-8');
+							fileReader.onloadend = function(evt) {
+								resovle(JSON.parse(evt.target.result));
+							}
+						});
+					});
+				});
+				
+			}catch(e){
+				reject(e);
+			}
+		})
+	}
+}
+
